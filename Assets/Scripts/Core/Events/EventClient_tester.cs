d6d677c28aeb927b13db4ce514521c509b037ae8
Marc Freixas <marc.freixas@gmail.com>
2015-07-09 23:31:30 +0200

    events added

---
+++ Assets/Scripts/Core/Events/EventClient_tester.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventClient_tester : MonoBehaviour
+{
+	public string message;
+
+	void OnEnable()
+	{
+		Events.instance.AddListener<MyCustomEvent>( OnCustomEventDone );
+	}
+
+	void OnDisable()
+	{
+		Events.instance.RemoveListener<MyCustomEvent>( OnCustomEventDone );
+	}
+
+	public void OnCustomEventDone( MyCustomEvent e )
+	{
+		Debug.Log( "event: " + message );
+	}
+}
---
+++ Assets/Scripts/Core/Events/EventGenerator_tester.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCustomEvent: GameEvent {}
+
+public class EventGenerator_tester : MonoBehaviour
+{
+	private const float REFRESH_TIME = 5.0f;
+	private float currentTime;
+
+
+	void Start ()
+	{
+		currentTime = REFRESH_TIME;
+	}
+	
+	void Update ()
+	{
+		currentTime -= Time.deltaTime;
+		if ( currentTime < 0.0f ) {
+			Events.instance.Raise( new MyCustomEvent() );
+			currentTime = REFRESH_TIME;
+		}
+	}
+}
